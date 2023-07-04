010bb4e8cc065c78ac7286bbb5cd4497d624e154
nastri05 <hoangtuan.t16@gmail.com>
2023-07-04 10:32:51 +0000

    update

--- System_call/proces_control/fork/exm_2_fork.c
+++ System_call/proces_control/fork/exm_2_fork.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+int value;
 void forkexample()
 {
     // child process because return value zero
     if (fork() == 0)
-    {
+    {	
         printf("Hello from Child!\n");
+		printf("Gia tri child before la : %d\n",&value);
+		value = 15;
+		printf("Gia tri child after la : %d\n",&value);
 	return;
     }
   
     // parent process because return value non-zero.
     else
     {
+		value = 10;
+		printf("Gia tri parent before la : %d\n",&value);
 		//	int value =fork();
-		printf("Value of fork = %d \n",fork() );
+//		printf("Value of fork = %d \n",fork() );
         printf("Hello from Parent!\n");
+		wait(NULL);
+		printf("Gia tri parent after la : %d\n",value);
 	return;
     }
 }
--- static-dynamic-library/app.c
+++ static-dynamic-library/app.c
@@ -4,6 +4,7 @@ int main(){
 	printf("Init foo = %d\n", getFoo());
 	setFoo(5);
 	printf("New foo = %d\n", getFoo());
+	while(1);
 	return 0;
 //
 }
